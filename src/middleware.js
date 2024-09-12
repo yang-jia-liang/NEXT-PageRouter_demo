5ea2c8cfa2e201d79cd394fa8c991b326d94fc3c
yangjialiang <528748995@qq.com>
2024-09-13 07:55:54 +0800

    update

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,12 @@ import { NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    // if (request.nextUrl.pathname.startsWith('/photo')) {
-    //     return NextResponse.rewrite(new URL('/photo/1', request.url))
-    // }
+    // console.log('middleware');
+    // console.log('pathname', request.nextUrl.pathname);
+
+    if (request.nextUrl.pathname.startsWith('/photo')) {
+        console.log('middleware redirect');
+        console.log(request.url);
+        return NextResponse.rewrite(new URL('/photo/1', request.url))
+    }
 }
--- src/pages/photo/[id].js
+++ src/pages/photo/[id].js
@@ -1,21 +1,22 @@
 import Image from "next/image";
 import image_bear from '@/assets/images/bear.jpg'
 import image_transport from '@/assets/images/transport.jpg'
-import {useParams} from "next/navigation";
-import {useEffect, useState} from "react";
+import { useParams, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function PhotoPage () {
     const params = useParams() ;
     const [photo, setPhoto] = useState()
-
     const photos = [
         { id: '1', value: image_bear },
         { id: '2', value: image_transport },
     ]
 
+    console.log(params || 'params not found');
+
     useEffect(() => {
         if (params) {
-            console.log({params});
+            console.log({ params });
 
             const photo = photos.find(p => p.id === params.id) || photos[0]
             setPhoto(photo.value)
@@ -23,7 +24,10 @@ export default function PhotoPage () {
 
     }, [params])
 
-    console.log({params});
+    const search = useSearchParams();
+    const searchParams = new URLSearchParams(search.toString())
+    const query = searchParams.toString()
+    console.log(query || 'query not found');
 
     return (
         <div>
